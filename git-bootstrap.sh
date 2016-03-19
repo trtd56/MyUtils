72e2f97f3490f33a0a099563c7a76214fceaaf71
TakamichiToda56 <5toda6@gmail.com>
2016-03-19 22:29:50 +0900

    gitのalias追加

---
+++ git-bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config --global user.name "Takamichi Toda"
+git config --global user.email "toda@hogehoge.com"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+#git config --global push.default simple
+git config --global core.precomposeunicode true
+git config --global core.quotepath false
