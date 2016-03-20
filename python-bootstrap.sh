e0185672d999b826459d4869c756eb81ac488787
TakamichiToda56 <5toda6@gmail.com>
2016-03-20 09:58:43 +0000

    pythonの初期設定

---
+++ python-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip install --upgrade pip
+
+#yes | sudo apt-get install libatlas-base-dev gfortran
+
+pip install numpy
+pip install ipython
+pip install chainer
+
+pip install matplotlib
+pip install scikit-learn
+pip install scipy
