171eb747e3700f261335f6a0be2d3b0ef7eb5e6e
Takamichi Toda <toda@hogehoge.com>
2016-04-15 02:17:43 +0000

    vagrantの設定

--- python/ipython-bootshrap.sh
+++ python/ipython-bootshrap.sh
@@ -9,3 +9,13 @@ echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.password = u'$PASSWD'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8888" >> ~/.jupyter/jupyter_notebook_config.py
+
+echo "/usr/local/bin/jupyter notebook --notebook-dir=~/share" > ~/start_jupyter.sh
+chmod 777 ~/start_jupyter.sh
+
+# Please edit
+#   /etc/rc.local
+# add
+#   su - vagrant /home/vagrant/start_jupyter.sh &
+# before
+#   exit 0
---
+++ vagrant/VagrantUp.bat
@@ -0,0 +1,7 @@
+cd C:\Users\toda.takamichi\vagrant
+
+vagrant up
+
+TIMEOUT -1
+
+vagrant halt
